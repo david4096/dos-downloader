27626e4f8307423b4c2c327483b0c148eda67681
David Steinberg <david@resium.com>
2017-12-05 23:10:55 -0800

    Add a basic dos downloader

---
+++ README.md
@@ -0,0 +1,12 @@
+# DOS Downloader
+
+A CLI client to download data from the DOS registry
+
+`dos-downloader dos-api data-object-id`
+
+## TODO
+
+* Add more downloading options
+* Use S3 and other storage clients
+* Authorization handling
+
---
+++ ga4gh/downloader/download.py
@@ -0,0 +1,40 @@
+import argparse
+import requests
+import sys
+
+from ga4gh.dos.client import Client
+
+def download_file(url, filename):
+    # NOTE the stream=True parameter
+    r = requests.get(url, stream=True)
+    with open(filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            sys.stdout.write(".")
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+    print('done')
+    return filename
+
+
+
+
+def download(url, id):
+    """Download data from a DOS service."""
+    local_client = Client(url)
+    client = local_client.client
+    models = local_client.models
+    data_object = client.GetDataObject(data_object_id=id).result().data_object
+    return download_file(data_object.urls[0].url, data_object.name)
+
+def main(args=None):
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('url',
+                        help='an integer for the accumulator')
+    parser.add_argument('id',
+                        help='sum the integers (default: find the max)')
+    args = parser.parse_args(args)
+    download(args.url, args.id)
+
+
+if __name__ == '__main__':
+    main()
